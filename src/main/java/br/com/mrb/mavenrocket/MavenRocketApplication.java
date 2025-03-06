edf2ed98fd852cef1b92551ba7939d547f82a0fd
mauro <mrb0305@gmail.com>
2025-03-06 10:00:23 -0300

    Primeiro commit

---
+++ src/main/java/br/com/mrb/mavenrocket/MavenRocketApplication.java
@@ -0,0 +1,13 @@
+package br.com.mrb.mavenrocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenRocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenRocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mrb/mavenrocket/MavenRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrb.mavenrocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenRocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
